00cf66f215c9ac6ff9b27305d335dc2836c5d8a7
Danton da Rosa Abreu <danton.rosa07@gmail.com>
2022-06-27 14:27:23 -0300

    contole_estoque.php

--- controle_estoque.php
+++ controle_estoque.php
@@ -5,6 +5,11 @@ include_once "conexao.php";
 if (!isset($_SESSION)) {
   session_start();
 }
+
+$sql = "SELECT * FROM item ORDER BY id ASC";
+$result_sql = mysqli_query($conexao, $sql);
+
+print_r($result_sql);
 ?>
 
 <body>
@@ -22,27 +27,51 @@ if (!isset($_SESSION)) {
     <a class="pesquisar-item" href="">
       <p>Pesquisar item</p>
     </a>
-    <label for="">
+
+    <table class="table">
+      <thread>
+        <tr class="primeira-linha">
+          <th>#</th>
+          <th>Nome</th>
+          <th>Tamanho</th>
+          <th>Disponib.</th>
+          <th>Ações</th>
+        </tr>
+      </thread>
+      <tbody>
+        <?php
+        while ($result_item = mysqli_fetch_assoc($result_sql)) {
+          echo "<tr>";
+          echo "<td>" . $result_item['id'] . "</td>";
+          echo "<td>" . $result_item['nome'] . "</td>";
+          echo "<td>" . $result_item['tamanho'] . "</td>";
+          echo "<td>" . $result_item['entrada'] - $result_item['saida'] . "</td>";
+          echo "<td>
+              <a href='edit_estoque.php?id=$result_item[id]'>
+                  <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil-square' viewBox='0 0 16 16'>
+                    <path d='M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z'/>
+                    <path fill-rule='evenodd' d='M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z'/>
+                </svg>
+              </a>
+            </td>";
+          echo "</tr>";
+        }
+        ?>
+      </tbody>
+    </table>
+
+    <button class="btn-enviar" type="submit">
+      <a id="enviar">Avançar</a>
+    </button>
+
+  </form>
+  <!-- <label for="">
       <span>Tipo item</span>
       <select name="id_categoria" id="id_categoria">
         <option value="">Escolha o tipo de item</option>
-        <?php
-        $result_cat_post = "SELECT * FROM categoria ORDER BY id";
-        $resultado_cat_post = mysqli_query($conexao, $result_cat_post);
-        while ($row_cat_post = mysqli_fetch_assoc($resultado_cat_post)) {
-          echo '<option value="' . $row_cat_post['id'] . '">' . $row_cat_post['nome'] . '</option>';
-          $v1 = $row_cat_post['id'];
-          $v2 =  $row_cat_post['nome'];
-        }
-        ?>
-
       </select>
     </label>
     <label>
-      <?php
-      echo $v1;
-      echo $v2;
-      ?>
       <span>Tamanho</span>
       <span class="carregando">Aguarde, carregando...</span>
       <select name="id_sub_categoria" id="id_sub_categoria">
@@ -60,14 +89,9 @@ if (!isset($_SESSION)) {
         <span>Saída</span>
         <input placeholder="" type="number" name="quantidade-saida" id="quantidade-saida" class="validate" />
       </label>
-    </div>
+    </div> -->
 
-    <button class="btn-enviar" type="submit">
-      <a id="enviar">Avançar</a>
-    </button>
-
-  </form>
-  <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <!-- <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script type="text/javascript">
     google.load("jquery", "1.4.2");
   </script>
@@ -94,7 +118,7 @@ if (!isset($_SESSION)) {
         }
       });
     });
-  </script>
+  </script> -->
 
 </body>
 
---
+++ edit_estoque.php
@@ -0,0 +1,88 @@
+<?php
+include_once "cabecalho.php";
+include_once "conexao.php";
+
+$nome = "";
+$tamanho = "";
+$msg = "";
+$quantidade_entrada = null;
+$quantidade_entrada = null;
+if (!isset($_SESSION)) {
+  session_start();
+}
+if (!empty($_GET['id'])) {
+  $id = $_GET['id'];
+  $sql_select_item  = "SELECT * FROM item WHERE id = $id";
+  $result_sql_select_item = mysqli_query($conexao, $sql_select_item);
+  if ($result_sql_select_item->num_rows > 0) {
+    while ($result_item = mysqli_fetch_assoc($result_sql_select_item)) {
+      $nome = $result_item['nome'];
+      $tamanho = $result_item['tamanho'];
+      $entrada = $result_item['entrada'];
+      $saida = $result_item['saida'];
+    }
+  } else {
+    header('Location: controle_estoque.php');
+  }
+
+  if (isset($_POST["quantidade-entrada"], $_POST["quantidade-saida"])) {
+    $quantidade_entrada = filter_input(INPUT_POST, "quantidade-entrada", FILTER_SANITIZE_NUMBER_INT);
+    $quantidade_saida = filter_input(INPUT_POST, "quantidade-saida", FILTER_SANITIZE_NUMBER_INT);
+    if ($quantidade_entrada >= 0 || $quantidade_saida >= 0) {
+      echo $quantidade_entrada;
+      echo $quantidade_saida;
+
+      $quantidade_entrada = $entrada + $quantidade_entrada;
+      $quantidade_saida = $saida + $quantidade_saida;
+      $sql_update_item = "UPDATE item SET entrada='$quantidade_entrada', saida='$quantidade_saida' WHERE id = '$id'";
+      $result_sql_update_item = mysqli_query($conexao, $sql_update_item);
+      $_SESSION['msg'] = "<p style='color:green;'>Enviado com sucesso!</p>";
+      $msg = $_SESSION['msg'];
+      header('Location: controle_estoque.php');
+    }
+  }
+}
+?>
+
+<body>
+  <header>
+    <nav class="navbar">
+      <a class="logo" href="tela_inicial.php">Make Good</a>
+      <ul class="nav-list">
+        <li>
+          <a href="controle_estoque.php">Voltar</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  <main class="container">
+    <form action="" method="POST">
+      <?php
+      echo "<h1>Efetue entrada ou saida em " . $nome . ",  tamanho: " . $tamanho . "</h1>";
+      echo "<h1>Disponibilidade = " . $entrada - $saida . "</h1>";
+      ?>
+      <div class="entrada-saida">
+        <label class="label-select" for="">
+          <span>Entrada</span>
+          <?php
+          echo "<input min='0' type='number' name='quantidade-entrada' id='quantidade-entrada' class='validate' value ='0' <input/>";
+          ?>
+        </label>
+
+        <label for="">
+          <span>Saída</span>
+          <?php
+          echo "<input min='0' type='number' name='quantidade-saida' id='quantidade-saida' class='validate' value ='0' <input/>";
+          ?>
+        </label>
+      </div>
+      <button class="btn-enviar" type="submit">
+        <a id="enviar">Enviar</a>
+      </button>
+      <?php
+      echo $msg;
+      ?>
+    </form>
+  </main>
+
+  </html>
